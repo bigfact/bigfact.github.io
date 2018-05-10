00e9d81ac97809039deecf3dc3d0feb2bf712b12
bigfact <bigfact@foxmail.com>
2018-05-11 00:07:43 +0800

    更新

---
+++ js/.prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * Prettier - Code formatter - 插件配置，可复制到单个项目中，仅对该项目有效
+ * @module js/_prettierrc
+ * @author bigfact
+ */
+
+module.exports = {
+  singleQuote: true,
+  printWidth: 640,
+  semi: false
+}
