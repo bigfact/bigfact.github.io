141455a964c4f79996c9c2166dc3aedb1e64d6ab
bigfact <bigfact@foxmail.com>
2019-05-12 22:22:19 +0800

    add waid dist files

---
+++ waid/precache-manifest.541dde4475640cd5e24ee7277ec30a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18cfd128cf2f7211980d9591004ddec",
+    "url": "/waid/index.html"
+  },
+  {
+    "revision": "323e016a084b19fff466",
+    "url": "/waid/static/css/2.8d32a9c8.chunk.css"
+  },
+  {
+    "revision": "0315255dabd0bd4a8060",
+    "url": "/waid/static/css/main.991388f8.chunk.css"
+  },
+  {
+    "revision": "323e016a084b19fff466",
+    "url": "/waid/static/js/2.fad9f43b.chunk.js"
+  },
+  {
+    "revision": "0315255dabd0bd4a8060",
+    "url": "/waid/static/js/main.fb105afa.chunk.js"
+  },
+  {
+    "revision": "ffa7bcccb4fa0994a7b1",
+    "url": "/waid/static/js/runtime~main.537b6995.js"
+  }
+]);
