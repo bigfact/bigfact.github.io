294fb8f7951219fb9d7d10b164fc82c788810eeb
leon <leon@localhost>
2020-06-09 23:31:27 +0800

    publish waid v0.7.0

--- waid/precache-manifest.7d73b526e34894eb43b349aa9fdae969.js
+++ waid/precache-manifest.7d73b526e34894eb43b349aa9fdae969.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4da1d3c9de2e131b9e2a8f0040a5cd0",
+    "revision": "5349981a091245d2b4c11012b6273dc1",
     "url": "/waid/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/waid/static/css/2.8a1acb70.chunk.css"
   },
   {
-    "revision": "864b719f1578bbdfe944",
-    "url": "/waid/static/css/main.ab226cc6.chunk.css"
+    "revision": "eab7b7dee2bae8958a04",
+    "url": "/waid/static/css/main.7e800051.chunk.css"
   },
   {
     "revision": "f9b947f7229270b843fc",
     "url": "/waid/static/js/2.49f177fd.chunk.js"
   },
   {
-    "revision": "864b719f1578bbdfe944",
-    "url": "/waid/static/js/main.570fb229.chunk.js"
+    "revision": "eab7b7dee2bae8958a04",
+    "url": "/waid/static/js/main.e41c1f59.chunk.js"
   },
   {
     "revision": "ffa7bcccb4fa0994a7b1",
--- waid/service-worker.js
+++ waid/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/waid/precache-manifest.b7e08d062e40866dfe300d8a4b42bd99.js"
+  "/waid/precache-manifest.7d73b526e34894eb43b349aa9fdae969.js"
 );
 
 self.addEventListener('message', (event) => {
